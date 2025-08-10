e78c743148204f8a6e2168cea9e03df3191e8b5a
rlriederer7 <rlriederer7@gmail.com>
2025-08-10 19:18:31 -0400

    Project Started

---
+++ src/main/java/org/morgan/untilmoraleimproves2/UntilMoraleImproves2Application.java
@@ -0,0 +1,13 @@
+package org.morgan.untilmoraleimproves2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntilMoraleImproves2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntilMoraleImproves2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/morgan/untilmoraleimproves2/UntilMoraleImproves2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.morgan.untilmoraleimproves2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntilMoraleImproves2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
