a4e1a726fba4ff6e05a0db6899f5b9ebd03c5abc
rlriederer7 <rlriederer7@gmail.com>
2025-09-03 19:22:15 -0400

    Added coefficient retrieval functionality to backend

--- backend/src/main/java/org/morgan/untilmoraleimproves2/client/PredictionClient.java
+++ backend/src/main/java/org/morgan/untilmoraleimproves2/client/PredictionClient.java
@@ -2,7 +2,9 @@ package org.morgan.untilmoraleimproves2.client;
 
 import org.morgan.untilmoraleimproves2.dto.ChurnPredictionRequest;
 import org.morgan.untilmoraleimproves2.dto.ChurnPredictionResponse;
+import org.morgan.untilmoraleimproves2.dto.CoefficientsResponse;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
@@ -10,4 +12,7 @@ import org.springframework.web.service.annotation.PostExchange;
 public interface PredictionClient {
     @PostExchange("/predict")
     ChurnPredictionResponse predictChurn(@RequestBody ChurnPredictionRequest request);
+
+    @GetExchange("/coefficients")
+    CoefficientsResponse getCoefficients();
 }
--- backend/src/main/java/org/morgan/untilmoraleimproves2/controller/ChurnController.java
+++ backend/src/main/java/org/morgan/untilmoraleimproves2/controller/ChurnController.java
@@ -3,6 +3,7 @@ package org.morgan.untilmoraleimproves2.controller;
 import org.morgan.untilmoraleimproves2.client.PredictionClient;
 import org.morgan.untilmoraleimproves2.dto.ChurnPredictionRequest;
 import org.morgan.untilmoraleimproves2.dto.ChurnPredictionResponse;
+import org.morgan.untilmoraleimproves2.dto.CoefficientsResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +22,15 @@ public class ChurnController {
 //        System.out.println("Received request: " + request); // Add this
         ChurnPredictionResponse response = predictionClient.predictChurn(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/coefficients")
+    public ResponseEntity<CoefficientsResponse> getCoefficients() {
+        try {
+            CoefficientsResponse coefficients = predictionClient.getCoefficients();
+            return ResponseEntity.ok(coefficients);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
